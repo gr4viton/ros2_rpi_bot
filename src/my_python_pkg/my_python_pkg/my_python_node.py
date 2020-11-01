8c3f5351790998df7857dc77e94c40936e4ec2f7
gr4viton <lordmutty@gmail.com>
2020-11-01 22:30:54 +0100

    add params but not taken in account.

--- src/my_python_pkg/my_python_pkg/my_python_node.py
+++ src/my_python_pkg/my_python_pkg/my_python_node.py
@@ -5,13 +5,28 @@ class MyPythonNode(Node):
     def __init__(self):
         super().__init__("my_node_name")
 
+        self.declare_parameter('my_str')
+        self.declare_parameter('my_int')
+        self.declare_parameter('my_double_array')
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('my_str2', None),
+                ('my_int2', None),
+                ('my_double_array2', None)
+            ]
+        )
+
         self.get_logger().info("This node just says 'Hello'")
 
 def main(args=None):
     rclpy.init(args=args)
+
     node = MyPythonNode()
     rclpy.spin(node)
     node.destroy_node()
+
     rclpy.shutdown()
 
 if __name__ == "__main__":
